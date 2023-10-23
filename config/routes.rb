e0d1e3d5161c6d677a8c3a0f16b64ad200e04700
vituhonda <vitorutk@gmail.com>
2023-10-23 14:44:06 -0300

    feat(routes): declaring resources for tables created #10

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   get 'welcome/index'
-  resources :coins
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :coins
+
+
 end
