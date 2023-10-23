d1d6e600b37771fa0616ae85c812c7d4b951a237
vituhonda <vitorutk@gmail.com>
2023-10-23 16:57:51 -0300

    feat(project):added seed to automatically create coins with rails db:seed #18

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Coin.create!(
+  description: "Bitcoin",
+  acronym: "BTC",
+  url_img:"https://static.vecteezy.com/system/resources/previews/008/505/801/original/bitcoin-logo-color-illustration-png.png"
+)
+
+Coin.create!(
+  description: "Ethereum",
+  acronym: "ETH",
+  url_img:"https://cryptologos.cc/logos/ethereum-eth-logo.png"
+)
+
+Coin.create!(
+  description: "Bitcoin",
+  acronym: "BTC",
+  url_img:"https://cryptologos.cc/logos/dash-dash-logo.png"
+)
