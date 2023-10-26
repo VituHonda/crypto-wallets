bbe467d9a03c8f9ccbd4f07292ca72220f03ec9e
vituhonda <vitorutk@gmail.com>
2023-10-26 14:32:35 -0300

    build(cookie): created a cookie and made it appear on welcome view #34

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
-
-
   def index
+    cookies[:alou] = "Oi eu sou um cookie"
     @teste =  "Olá estou testando, sou uma variável de instância do controller welcome"
     @nome = params[:nome]
   end
