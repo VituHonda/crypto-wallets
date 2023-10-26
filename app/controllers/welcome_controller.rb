bb50256b23f930411bfcaa23ff1e026d5e5f560b
vituhonda <vitorutk@gmail.com>
2023-10-26 14:35:45 -0300

    build(session): created a session and made it appear on welcome view #35

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    session[:hi] = "Oi eu sou uma session"
     cookies[:alou] = "Oi eu sou um cookie"
     @teste =  "Olá estou testando, sou uma variável de instância do controller welcome"
     @nome = params[:nome]
